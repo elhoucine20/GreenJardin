583a83d1fba71e5de8da440095c3f906e06e5c5a
elhoucine20 <elhoucinenaitbrahim@gmail.com>
2026-04-02 19:41:37 +0100

    migration de paiments avec la relation entre commande et paiment

---
+++ app/Http/Controllers/client/PaimentController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\client;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PaimentController extends Controller
+{
+    //
+    public function index(){}
+}
